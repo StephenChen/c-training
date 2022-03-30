8a8b5f63b6a23770e7b99ae22b0822179358ee91
StephenChen <stephenchen1994@outlook.com>
2022-03-30 16:54:31 +0800

    UPDATE: complete A

---
+++ A/assignment18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void hello1(void) {
+    char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33};
+
+    printf("%s", array);
+}
+
+void hello2(void) {
+    char array[] = {72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33, '\0'};
+
+    printf("%s", array);
+}
+
+int main(void) {
+    hello1();
+    printf("\n");
+    hello2();
+}
